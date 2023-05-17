5221c0e65cfa37fe84003b1defb0b79995abff26
于龙 <yulong@foryou56.com>
2023-05-17 11:43:28 +0800

    fix

--- swift/source/util/TimedQueue.swift
+++ swift/source/util/TimedQueue.swift
@@ -24,45 +24,53 @@ final class TimedQueue<Key: Hashable> {
     typealias Element = (key: Key, clock: SteadyClock)
     typealias List = ContiguousArray<Element>
     typealias Map = [Key: List.Index]
-
+    
     let delay: TimeInterval
-
+    
     let conditionLock = ConditionLock()
-
+    
     var list: List = []
     var map: Map = [:]
-
+    
     init(withDelay delay: TimeInterval) {
         self.delay = delay
     }
-
+    
     func remove(with key: Key) {
         conditionLock.lock(); defer { conditionLock.unlock() }
-
+        
         guard let index = map.index(forKey: key) else {
             return
         }
-        list.remove(at: map[index].value)
+        /// 这里需要判断list和map对应的元素个数和map[index]的值以及index的值比较一下，
+        /// 不然会越界crash
+        if list.count >  map[index].value {
+            list.remove(at: map[index].value)
+        }
         map.remove(at: index)
     }
-
+    
     func reQueue(with key: Key) {
         conditionLock.lock(); defer { conditionLock.unlock() }
-
+        
         let signal = list.isEmpty
-
+        
         if let index = map.index(forKey: key) {
-            list.remove(at: map[index].value)
+            /// 这里需要判断list和map对应的元素个数和map[index]的值以及index的值比较一下，
+            /// 不然会越界crash
+            if list.count >  map[index].value {
+                list.remove(at: map[index].value)
+            }
             map.remove(at: index)
         }
-
+        
         list.append((key, SteadyClock.now()+delay))
         map[key] = list.startIndex
         if signal {
             conditionLock.signal()
         }
     }
-
+    
     func wait(untilExpired onExpired: (Key) -> Void) {
         while true {
             var key: Key!
